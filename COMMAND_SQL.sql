b2232667e439125ab30efab0509603880b2abd7f
WKBRAZIL\Hugo.Barros <hugo.barros@wolterskluwer.com>
2019-11-04 17:57:48 -0300

    Versão Final

---
+++ COMMAND_SQL.sql
@@ -0,0 +1,36 @@
+﻿CREATE DATABASE HsrtechDB
+
+GO
+
+USE HsrtechDB
+
+GO
+
+CREATE TABLE HistoricalTransaction(
+    HistoricalId int primary key IDENTITY(1,1),
+    NumberAccount int  NOT NULL,
+    Date DATETIME NOT NULL,
+    Value decimal NOT NULL,
+    FlagTransaction bit NOT NULL
+);
+
+GO
+
+CREATE TABLE Client(
+    ClientId int primary key,
+    Name varchar(250) NOT NULL,
+    Login varchar(250) NOT NULL,
+    Password varchar(250) NOT NULL,
+);
+
+GO
+
+CREATE TABLE BankAccount(
+    NumberAccount int primary KEY IDENTITY(1,1),
+    OpenDate DATETIME NOT NULL,
+    Balance decimal NOT NULL,
+    ClientId int NOT NULL,
+    Limit int NOT NULL,
+
+    CONSTRAINT Fk_Client FOREIGN KEY (ClientId) REFERENCES Client (ClientId)
+);
